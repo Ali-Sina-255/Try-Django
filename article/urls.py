d0ad44029be587bf13c9497a37edebb597586bff
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-12 14:54:47 +0430

    article create and detail view is created

--- article/urls.py
+++ article/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.article_create_view, name='article')
+    path('', views.article_create_view, name='article'),
+    path('article_detail/<int:value_from_url>/', views.article_detail_view, name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from . models import Articles
+from django.contrib import messages
 
 
 def article_create_view(request, *args, **kargs):
-    return render(request, 'index.html')
+
+    context = {}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article_object = Articles.objects.create(title=title, content=content)
+        context['object'] = article_object
+        context['created'] = True
+
+    return render(request, 'index.html', context=context)
+
+
+def article_detail_view(request, value_from_url, *args, **kargs):
+    articlce_detail = Articles.objects.get(id=value_from_url)
+
+    context = {
+        'articlce_detail': articlce_detail
+    }
+    return render(request, 'detail.html', context=context)

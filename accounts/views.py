37297aa362ac49f5152a3291353d0a83f9d30711
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-12 15:18:59 +0430

    account login view is created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_view(request, *args, **Kwargs):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            context = {
+                "error": 'Invalid username or passowrd'
+            }
+            return render(request, 'account/login.html', context)
+
+        login(request, user)
+        messages.success(request, 'you are login new')
+        return redirect('/')
+
+    return render(request, 'account/login.html', {})

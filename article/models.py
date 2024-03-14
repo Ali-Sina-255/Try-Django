d376727537cd5ab628b259d72bdc02e63efc2352
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-14 10:58:54 +0430

    save method is update

--- article/models.py
+++ article/models.py
@@ -1,37 +1,43 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.text import slugify
 from django.db.models.signals import post_save, pre_save
+from django.utils.text import slugify
 
+from . utils import slugify_instance_title
 
 class Articles(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField( unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    publish = models.DateField(auto_now_add=False, auto_now=False, default=timezone.now)
-    
-    def __str__(self) -> str:
+    publish = models.DateField(
+        auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if self.slug is None:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
 
-def article_pre_save(instance,sender, *args, **kwargs):
+
+
+
+def article_pre_save(instance, sender, *args, **kwargs):
     if instance.slug is None:
-        instance.slug=slugify(instance.title)
+        slugify_instance_title(instance, save=False)
+
 
 pre_save.connect(article_pre_save, sender=Articles)
 
 
-def article_post_save(instance,sender,created, *args, **kwargs):
+def article_post_save(instance, sender, created, *args, **kwargs):
     print('Post Save')
     if created:
-        instance.slug=slugify(instance.title)
-        instance.save()
-        
+        slugify_instance_title(instance, save=True)
+
+
 post_save.connect(article_post_save, sender=Articles)
---
+++ article/utils.py
@@ -0,0 +1,21 @@
+import random
+from django.utils.text import slugify
+
+
+def slugify_instance_title(instance, save=False, new_slug=None):
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+    Klass = instance.__class__
+    qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
+    if qs.exists():
+        # auto generate new slug
+        random_int = random.randint(300_000, 500_000)
+        slug = f'{slug}-{random_int}'
+        return slugify_instance_title(instance, save=False, new_slug=slug)
+
+    instance.slug = slug
+    if save:
+        instance.save()
+    return instance

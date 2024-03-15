037198cfbdf63376a7b3e49c234ec278002639a4
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-03-15 14:06:34 +0430

    manage custom queryset with modelformset factory

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,12 +1,23 @@
 from typing import Any
 from django.forms import ModelForm
-from . models import Recipes
+from . models import Recipes, RecipeIngredient
 
 
 class RecipesForm(ModelForm):
     class Meta:
         model = Recipes
         fields = ["name", "description", "directions"]
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        print("all clean data")
+        return cleaned_data
+
+
+class RecipeIngredientForm(ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ["name", "unit", "quantity"]
 
     def clean(self):
         cleaned_data = self.cleaned_data
--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from requests import delete
-from . models import Recipes
-from . forms import RecipesForm
-
+from . models import Recipes, RecipeIngredient
+from . forms import RecipesForm, RecipeIngredientForm
+from django.forms.models import modelformset_factory
 
 @login_required
 def recipe_list_view(request):
@@ -46,19 +46,35 @@ def recipe_update_views(request, value_from_url):
         Recipes, id=value_from_url, user=request.user)
     
     form = RecipesForm(request.POST or None, instance=object)
+    
+    RecipeIngredientFormSet = modelformset_factory(RecipeIngredient,form=RecipeIngredientForm,extra=0)
+    qs = object.recipeIngredient_set.all()
+    formset = RecipeIngredientFormSet(request.POST or None, queryset=qs)
+    
     context = {
         "object": object,
-        'form': form
+        'form': form,
+        "formset" :formset
     }
     if request.method == "POST":
         form = RecipesForm(request.POST, instance=object)
-        if form.is_valid():
-            form.save()
+        
+        if all([form.is_valid(), formset.is_valid()]):
+            parent= form.save(commit=False)
+            parent.save()
+            # formset.save()
+            for form in formset:
+                child = form.save(commit=False)
+                if child.recipe is None:   
+                    print('Added new')
+                    child.recipe = parent
+                child.save()
             context["message"] = "Data Saved"
             return redirect(object.get_recipe_list_url())
         else:
             print(form.errors)
             form = RecipesForm()
+            # form_2 = RecipeIngredientForm()
     
     return render(request, 'recipes/recipe_update.html', context)
 
